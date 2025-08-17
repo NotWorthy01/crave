6af6178d5ba5de66f46ad54832a4dca375d2fb86
LuCIFeR <goruchannel2010@gmail.com>
2025-08-17 12:58:10 +0530

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -6,6 +6,8 @@ https://github.com/NotWorthy01/device_xiaomi_haydn -b crd device/xiaomi/haydn
 
 . b*/e*
 
+curl -sSf https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script-auto/main/create-signed-env.sh | bash
+
 . b*/e*
 
 brunch haydn
