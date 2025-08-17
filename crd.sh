fd9acaf399ed028f096b98d86574b8cec6cedc88
LuCIFeR <goruchannel2010@gmail.com>
2025-08-17 20:12:55 +0530

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -1,5 +1,7 @@
 repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
 
+rm -rf prebuilts/clang/host/linux-x86
+
 /opt/crave/resync.sh
 
 git clone https://github.com/NotWorthy01/device_xiaomi_haydn -b crd device/xiaomi/haydn
