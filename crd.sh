a69278a91b187b7fad8ab25e91ecfefb57d36d46
LuCIFeR <goruchannel2010@gmail.com>
2025-08-17 12:57:28 +0530

    Create crd.sh

---
+++ crd.sh
@@ -0,0 +1,11 @@
+repo init -u https://github.com/crdroidx/android.git -b 16.0 --git-lfs --no-clone-bundle
+
+git clone 
+
+https://github.com/NotWorthy01/device_xiaomi_haydn -b crd device/xiaomi/haydn
+
+. b*/e*
+
+. b*/e*
+
+brunch haydn
