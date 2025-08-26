b46b7a503b619ff9106718ed686b36d3a0895bc2
NotWorthy01 <goruchannel2010@gmail.com>
2025-08-26 14:07:15 +0530

    ndjd

--- crd.sh
+++ crd.sh
@@ -8,14 +8,14 @@ rm -rf prebuilts/clang/host/linux-x86
 
 /opt/crave/resync.sh
 
-git clone https://github.com/NotWorthy01/device_xiaomi_haydn -b rising device/xiaomi/haydn
+git clone https://github.com/NotWorthy01/device_xiaomi_haydn -b axion device/xiaomi/haydn
 
 . b*/e*
 
 curl -sSf https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script-auto/main/create-signed-env.sh | bash
 
-gk -f 
+gk -s
 
-riseup haydn userdebug
+axion haydn gms core userdebug
 
-rise sb
+ax -br
