c7302fd173183314f958370e352d6d0b96561ace
LuCIFeR <goruchannel2010@gmail.com>
2025-08-31 12:32:42 +0530

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -8,7 +8,7 @@ rm -rf prebuilts/clang/host/linux-x86
 
 /opt/crave/resync.sh
 
-git clone https://github.com/NotWorthy01/device_xiaomi_haydn -b rise device/xiaomi/haydn
+git clone https://github.com/NotWorthy01/device_xiaomi_haydn -b rising device/xiaomi/haydn
 
 . build/envsetup.sh
 
