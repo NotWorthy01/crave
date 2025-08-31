b5e5d970b2623cd939a6246cc5955da6af172033
LuCIFeR <goruchannel2010@gmail.com>
2025-09-01 04:03:03 +0530

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -1,14 +1,14 @@
-repo init -u https://github.com/RisingOS-Revived/android -b sixteen-los --git-lfs
+#repo init -u https://github.com/RisingOS-Revived/android -b sixteen-los --git-lfs
 
-rm -rf prebuilts/clang/host/linux-x86
-
-/opt/crave/resync.sh
+#rm -rf prebuilts/clang/host/linux-x86
 
 /opt/crave/resync.sh
 
 /opt/crave/resync.sh
 
-git clone https://github.com/NotWorthy01/device_xiaomi_haydn -b rising device/xiaomi/haydn
+/opt/crave/resync.sh
+
+#git clone https://github.com/NotWorthy01/device_xiaomi_haydn -b rising device/xiaomi/haydn
 
 . b*/e*
 
