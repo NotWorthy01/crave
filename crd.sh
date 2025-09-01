d069599a49650b46e45bed7a07ab0551e884c561
NotWorthy01 <goruchannel2010@gmail.com>
2025-09-01 21:33:58 +0530

    hdnd

--- crd.sh
+++ crd.sh
@@ -1,21 +1,21 @@
-#repo init -u https://github.com/RisingOS-Revived/android -b sixteen-los --git-lfs
+repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16 --git-lfs
 
-#rm -rf prebuilts/clang/host/linux-x86
-
-/opt/crave/resync.sh
+rm -rf prebuilts/clang/host/linux-x86
 
 /opt/crave/resync.sh
 
 /opt/crave/resync.sh
 
-#git clone https://github.com/NotWorthy01/device_xiaomi_haydn -b rising device/xiaomi/haydn
+/opt/crave/resync.sh
+
+git clone https://github.com/NotWorthy01/device_xiaomi_haydn -b derp device/xiaomi/haydn
 
 . b*/e*
 
-gk -f
+#gk -f
 
 . build/envsetup.sh
 
-riseup haydn userdebug
+lunch lineage_haydn-bp2a-userdebug
 
-rise sb
+mka derp
