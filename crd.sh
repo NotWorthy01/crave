8b18554f023214dc6836fd889e55dc0c470501c6
NotWorthy01 <goruchannel2010@gmail.com>
2025-09-19 15:01:19 +0530

    fjf

--- crd.sh
+++ crd.sh
@@ -1,4 +1,4 @@
-repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
+repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16 --git-lfs
 
 rm -rf prebuilts/clang/host/linux-x86
 
@@ -8,7 +8,7 @@ rm -rf prebuilts/clang/host/linux-x86
 
 /opt/crave/resync.sh
 
-git clone https://github.com/NotWorthy01/device_xiaomi_haydn -b test device/xiaomi/haydn
+git clone https://github.com/NotWorthy01/device_xiaomi_haydn -b derp device/xiaomi/haydn
 
 source build/envsetup.sh
 
@@ -22,4 +22,6 @@ curl -sSf https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script
 
 . build/envsetup.sh
 
-brunch haydn
+lunch lineage_haydn-bp2a-userdebug
+
+mka derp
