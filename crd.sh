8b88d54fe47e02219ac550ac5048b8fecc965956
NotWorthy01 <goruchannel2010@gmail.com>
2025-09-21 17:44:26 +0530

    jdjdj

--- crd.sh
+++ crd.sh
@@ -16,6 +16,10 @@ rm -rf kernel/xiaomi/sm8350
 
 git clone https://github.com/Haydn-Lab/kernel_xiaomi_haydn -b lineage-22.2 kernel/xiaomi/sm8350
 
+rm -rf hardware/lineage/compat
+
+git clone https://github.com/NotWorthy01/android_hardware_lineage_compat -b 23 hardware/lineage/compat
+
 . b*/e*
 
 curl -sSf https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script-auto/main/create-signed-env.sh | bash
