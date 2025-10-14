c25a41b8ff644837671edd4a346c30ed89200286
L u C I F e R <goruchannel2010@gmail.com>
2025-10-14 22:46:47 +0530

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -1,4 +1,4 @@
-repo init -u https://github.com/RisingOS-Revived/android -b sixteen-los --git-lfs
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
 
 rm -rf prebuilts/clang/host/linux-x86
 
@@ -8,11 +8,9 @@ rm -rf prebuilts/clang/host/linux-x86
 
 /opt/crave/resync.sh
 
+git clone https://github.com/NotWorthy01/device_xiaomi_haydn -b crd device/xiaomi/haydn
+
 . b*/e*
-
-rm -rf kernel/xiaomi/sm8350
-
-git clone https://github.com/NotWorthy01/android_kernel_xiaomi_sm8350 -b lineage-22.2 kernel/xiaomi/sm8350
 
 rm -rf hardware/lineage/compat
 
@@ -20,21 +18,10 @@ git clone https://github.com/NotWorthy01/android_hardware_lineage_compat -b 23 h
 
 . b*/e*
 
-. b*/e*
 
-. b*/e*
-
-source build/envsetup.sh
-
-source build/make/envsetup.sh
-
-#curl -sSf https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script-auto/main/create-signed-env.sh | bash
-gk -f
+curl -sSf https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script-auto/main/create-signed-env.sh | bash
+#gk -f
 
 . build/envsetup.sh
 
-riseup haydn userdebug
-
-riseup haydn userdebug
-
-rise sb
+brunch haydn
