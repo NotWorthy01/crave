0ecbdf352e2a80d378c82d96f6329c5d10e6f98b
L u C I F e R <goruchannel2010@gmail.com>
2025-10-30 07:07:35 +0530

    Update crd.sh for new repository and build commands

--- crd.sh
+++ crd.sh
@@ -1,4 +1,4 @@
-repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16 --git-lfs
+repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
 
 rm -rf prebuilts/clang/host/linux-x86
 
@@ -8,7 +8,7 @@ rm -rf prebuilts/clang/host/linux-x86
 
 /opt/crave/resync.sh
 
-git clone https://github.com/NotWorthy01/device_xiaomi_haydn -b derp device/xiaomi/haydn
+git clone https://github.com/NotWorthy01/device_xiaomi_haydn -b rise device/xiaomi/haydn
 
 . b*/e*
 
@@ -18,11 +18,13 @@ git clone https://github.com/NotWorthy01/android_hardware_lineage_compat -b 23 h
 
 . b*/e*
 
-curl -sSf https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script-auto/main/create-signed-env.sh | bash
-#gk -f
+#curl -sSf https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script-auto/main/create-signed-env.sh | bash
+gk -f
 
 . build/envsetup.sh
+. build/envsetup.sh
+. build/envsetup.sh
 
-lunch lineage_haydn-bp2a-userdebug
+riseuo haydn userdebug
 
-mka derp
+rise sb
